7410efebd615b07dba8d1abadb2adbb76da4cc7e
Nigar Isali <nigarisali@Nigars-iMac.local>
2021-12-28 19:58:56 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <title>PHP</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <div class="d-1">
+
+
+        <h1>HALCYONDAYS</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <button>
+          Back TO TIME ARTICLE
+        </button>
+      </div>
+
+      <div class="d-2">
+        <h5>
+          A CREATIVE PORTFOLIO TEMPLATE<br>
+          <h1>
+            <i class="bi bi-chevron-down"></i>
+          </h1>
+        </h5>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit repellendus, repellat porro blanditiis,
+          laborum laudantium expedita iste exercitationem dolore, quae molestias! Officiis dolores perspiciatis aliquam?
+          Beatae laudantium accusamus quam et?
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus ea vel molestiae, amet reiciendis
+          numquam maxime animi in quam, cumque dolores, dicta iusto deleniti fuga labore est repellendus culpa
+          exercitationem?
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint excepturi a maiores adipisci accusantium totam
+          praesentium nemo aspernatur repudiandae. Commodi facere amet quos recusandae? Explicabo cumque dolor maiores
+          nisi cum?
+
+        </p>
+      </div>
+      <div class="d-3">
+        <h3>
+          LOVE WHAT YOU DO,AND YOU`LL DI IT WELL
+          <h1>
+            <i class="bi bi-chevron-down"></i>
+          </h1>
+        </h3>
+      </div>
+      <div class="d-4">
+        <div class="row">
+          <div class="col-xl-4">
+            <button class="a">
+              <h1>
+                <i class="bi bi-laptop"></i>
+              </h1>
+            </button>
+            <h5>Digital Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis magnam vel excepturi magni dicta at ut
+              rerum, ab eos debitis neque libero repellendus omnis aperiam nam! At maiores omnis et?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis magnam vel excepturi magni dicta at ut
+              rerum, ab eos debitis neque libero repellendus omnis aperiam nam! At maiores omnis et?
+
+            </p>
+          </div>
+
+          <div class="col-xl-4">
+            <button class="b">
+              <h1>
+                <i class="bi bi-chevron-bar-expand"></i>
+              </h1>
+            </button>
+            <h5>Digital Design</h5>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt obcaecati debitis nostrum? Doloribus
+              minima minus, sed ut nisi quisquam expedita dolores eaque blanditiis fugiat iure, quidem odit adipisci cum
+              incidunt!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis magnam vel excepturi magni dicta at ut
+              rerum, ab eos debitis neque libero repellendus omnis aperiam nam! At maiores omnis et?
+
+            </p>
+          </div>
+          <div class="col-xl-4">
+            <button class="c">
+              <h1>
+                <i class="bi bi-heart"></i>
+              </h1>
+            </button>
+            <h5>Digital Design</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae maiores eum iure modi deserunt.
+              Sapiente eum voluptate ducimus atque, illo nostrum quis, repellat, eaque nam distinctio perferendis beatae
+              maxime totam?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis magnam vel excepturi magni dicta at ut
+              rerum, ab eos debitis neque libero repellendus omnis aperiam nam! At maiores omnis et?
+
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="d-5">
+        <div class="row">
+            <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+<img class="img1" src="images.jpg">
+            </div>
+        <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+          
+          <h1>
+            PROFESSIONALS
+
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt illum saepe iure quod culpa nisi?
+            Tempora nisi id sunt quod doloremque, excepturi tempore distinctio iste incidunt deleniti fuga vero.</p>
+           
+     
+      </div>
+      <div class="col-12 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+         <h1>
+              PRESPONSIVE D
+  
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia incidunt illum saepe iure quod culpa nisi?
+              Tempora nisi id sunt quod doloremque, excepturi tempore distinctio iste incidunt deleniti fuga vero.</p>
+      </div>
+</div>
+      </div>
+
+    </div>
+  </header>
+
+
+
+
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+    integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+    integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
